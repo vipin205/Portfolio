bc59906491958ea6a3f1df808ed5bd78fc3efddd
Vipin Kumar Yadav <kumaryadavv582@gmail.com>
2025-11-20 23:49:08 +0530

    ,

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description:
     "My name is Vipin Kumar Yadav. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: "kumaryadavv582@gmail.com",
-  phone: "+9987285431",
+  phone: "+919987285431",
   address: "Varanasi , 221206 ",
   github: "https://github.com/vipin205",
   facebook: "https://www.facebook.com/profile.php?id=100049989398296",
